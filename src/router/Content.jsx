b9e29a4f185d3b5efe2b4dee78a9c0dca6c46986
Gfrey <gfreycode@gmail.com>
2023-02-09 16:38:27 -0500

    delete

--- src/router/Content.jsx
+++
@@ -1,9 +0,0 @@
-import title_img from '../assets/Titulo.png'
-
-export function Content() {
-    return <>
-        <div>
-            <img className="img_title slct" src={title_img}/>
-        </div>
-    </>
-}
