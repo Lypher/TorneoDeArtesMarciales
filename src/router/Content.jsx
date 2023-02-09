f7a18210bc3709c4f3f83fbe3967e70de04711d8
Gfrey <gfreycode@gmail.com>
2023-02-09 17:05:39 -0500

    merge

---
+++ src/router/Content.jsx
@@ -0,0 +1,9 @@
+import title_img from '../assets/Titulo.png'
+
+export function Content() {
+    return <>
+        <div>
+            <img className="img_title slct" src={title_img}/>
+        </div>
+    </>
+}
