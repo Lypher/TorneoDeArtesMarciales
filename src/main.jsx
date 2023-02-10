496ed1703a790ab7d4b6429b84a982cfe550ee8a
enokyw <enokyws@gmail.com>
2023-02-10 00:52:46 -0500

    add sound

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import './animations.css'
 import { App_router } from './router/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
